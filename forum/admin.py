c568f3f34329e098f5a2849458dbe1df8c6f4f60
Agustin cilli <cilli.agustin@gmail.com>
2022-12-23 18:31:23 +0000

    Create basic model for post and comment + register admin

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title']
+    list_filter = ('status', 'created_on')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('created_on', 'approved')
+    search_fields = ['name', 'email', 'body']
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+
+STATUS = ((0, 'Unpublished'), (1, 'Published'))
+CATEGORY = (('Button', 'Button'), ('Navbar', 'Navbar'), ('Card', 'Card'), ('Miscellaneous', 'Miscellaneous'))
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='forum_posts')
+    updated_date = models.DateTimeField(auto_now=True)
+    html_content = models.TextField()
+    css_content = models.TextField()
+    js_content = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    category = models.IntegerField(choices=CATEGORY, default='Miscellaneous')
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments"
+    )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
