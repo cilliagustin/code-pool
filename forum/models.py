64b005189bbc33fedfb744c9e2b39c618562d0c7
Agustin cilli <cilli.agustin@gmail.com>
2022-12-24 16:56:16 +0000

    Add views to use templates

--- forum/models.py
+++ forum/models.py
@@ -30,7 +30,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments"
+        Post, on_delete=models.CASCADE, related_name='comments'
     )
     name = models.CharField(max_length=80)
     email = models.EmailField()
@@ -42,4 +42,4 @@ class Comment(models.Model):
         ordering = ['created_on']
 
     def __str__(self):
-        return f"Comment {self.body} by {self.name}"
+        return f'Comment {self.body} by {self.name}'
