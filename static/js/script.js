63e9654acf7885db570c519fdbe9bdb76d6b3091
Agustin cilli <cilli.agustin@gmail.com>
2022-12-26 20:00:38 +0000

    Add basic structure to main banner in index

--- static/js/script.js
+++ static/js/script.js
@@ -35,3 +35,69 @@ function addYear(){
 window.onload = addYear
 
 
+
+// #### INDEX ####
+
+// Typewritter effect
+class TypeWriter {
+    constructor(titleWord, words) {
+      this.titleWord = titleWord;
+      this.words = words;
+      this.txt = '';
+      this.wordIndex = 0;
+      this.type();
+      this.isDeleting = false;
+    }
+  
+    type() {
+      const current = this.wordIndex % this.words.length;
+      const fullTxt = this.words[current];
+  
+      // Check if deleting
+      if(this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+      } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+      }
+  
+      // Insert txt into element
+      this.titleWord.innerHTML = this.txt;
+  
+      // Initial Type Speed
+      let typeSpeed = 300;
+  
+      if(this.isDeleting) {
+        typeSpeed /= 2;
+      }
+  
+      // If word is complete
+      if(!this.isDeleting && this.txt === fullTxt) {
+        // Make pause at end
+        typeSpeed = 3000;
+        // Set delete to true
+        this.isDeleting = true;
+      } else if(this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        // Move to next word
+        this.wordIndex++;
+        // Pause before start typing
+        typeSpeed = 500;
+      }
+  
+      setTimeout(() => this.type(), typeSpeed);
+    }
+  }
+  
+  
+  // Init On DOM Load
+  document.addEventListener('DOMContentLoaded', init);
+  
+  // Init App
+  function init() {
+    const titleWord = document.getElementById("title-word");
+    const words = ["code", "ideas", "knowledge"];
+    // Init TypeWriter
+    new TypeWriter(titleWord, words);
+  }
+
+
