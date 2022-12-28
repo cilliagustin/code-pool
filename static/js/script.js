9014904326e474f87491f41edb28c278f388b5f2
Agustin cilli <cilli.agustin@gmail.com>
2022-12-28 20:30:20 +0000

    Add function to manupilate code editor + conect with function to display code

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,16 @@
+const mediumMediaQuery = window.matchMedia('(min-width: 576px)');
+const largeMediaQuery = window.matchMedia('(min-width: 768px)');
 const navbar = document.querySelector("nav");
 const navButton = document.getElementById("hamb");
 const overlay = document.getElementById("overlay");
+const codeEditor = document.getElementById("code-editor");
+const codeEditorBtns = document.querySelectorAll(".code-editor-btn");
 
+
+// ##### GENERAL FUNCTIONS #####
 
 // #### NAVBAR ####
-// #### Navbar collapse ####
-
+//  Navbar collapse 
 navButton.addEventListener("click", () => {
   if (navbar.getAttribute("data-open") === "false") {
     navbar.setAttribute("data-open", "true");
@@ -17,15 +22,13 @@ navButton.addEventListener("click", () => {
 });
 
 // close navbar at 768px
-const mediaQuery = window.matchMedia('(min-width: 768px)');
-
-function windowResize(mediaQuery) {
-  if (mediaQuery.matches) {
+function navbarWindowResize(largeMediaQuery) {
+  if (largeMediaQuery.matches) {
     navbar.setAttribute("data-open", "false");
     overlay.setAttribute("data-open", "false");
   }
 };
-mediaQuery.addEventListener('change', windowResize);
+largeMediaQuery.addEventListener('change', navbarWindowResize);
 
 // #### FOOTER ####
 // add year to footer
@@ -33,14 +36,23 @@ function addYear() {
   document.getElementById("footerYear").innerHTML = new Date().getFullYear();
 }
 
+// #### CODE EDITOR FUNCTION ####
+function runEditor(iframe, htmlCode, cssCode, jsCode) {
+  const code = `${htmlCode} <style>${cssCode}</style><script>${jsCode}</script>`
+  const iframeContent = iframe.contentWindow.document;
+  iframeContent.open();
+  iframeContent.write(code);
+  iframeContent.close();
+}
+
+// #### init general functions
 // Init addYear On DOM Load
 document.addEventListener('DOMContentLoaded', addYear);
 
 
+// ##### INDEX #####
 
-// #### INDEX ####
-
-// Typewritter effect
+// #### Typewritter effect ####
 class TypeWriter {
   constructor(titleWord, words) {
     this.titleWord = titleWord;
@@ -93,30 +105,24 @@ class TypeWriter {
 function initTypewriter() {
   const titleWord = document.getElementById("title-word");
   const words = ["code", "ideas", "knowledge"];
-  // Init TypeWriter
   new TypeWriter(titleWord, words);
 }
 
-
-
-// Display preview cards
+// #### Display preview cards ####
 function displayPreviewCards(){
   let previewCards = document.querySelectorAll(".preview-card");
   previewCards.forEach(card =>{
-    let htmlCode = card.querySelector(".preview-card-body [data-code-html]").innerText;
-    let cssCode = card.querySelector(".preview-card-body [data-code-css]").innerText;
-    let jsCode = card.querySelector(".preview-card-body [data-code-js]").innerText;
+    let htmlCode = card.querySelector(".preview-card-body [data-code-html]").value;
+    let cssCode = card.querySelector(".preview-card-body [data-code-css]").value;
+    let jsCode = card.querySelector(".preview-card-body [data-code-js]").value;
     let iframe = card.querySelector(".preview-card-body iframe");
     runEditor(iframe, htmlCode, cssCode, jsCode)
   })
 }
 
-
-
-
-//init index functions
+//#### Init index functions ####
 function runIndexFunctions(){
-  if(!document.querySelector(".main-banner") === null){
+  if(document.querySelector(".main-banner") !== null){
     initTypewriter()
     displayPreviewCards()
   }
@@ -125,19 +131,108 @@ function runIndexFunctions(){
 document.addEventListener('DOMContentLoaded', runIndexFunctions);
 
 
+// ##### POST DETAILS #####
 
+// #### CODE EDITOR BUTTON CONTROL ####
+codeEditorBtns.forEach(btn =>{
+  btn.addEventListener("click", ()=>{
+    toggleEditor(btn)
+  })
+})
 
+// toggle open/close code editor
+function toggleEditor(codeBtn){
+  const dataCodeType = codeBtn.getAttribute("data-code-type");
+  const codeElements = document.querySelectorAll(`[data-code-type=${dataCodeType}]`)
+  
 
+  //Toggle open or close elements (1 element open at the time) when width is less than 576px
+  if (!mediumMediaQuery.matches){
+    document.querySelectorAll("[data-code-type]").forEach(el =>{
+      if(el.getAttribute("data-code-type") === dataCodeType){
+        if(el.getAttribute("data-open") === "true"){
+          el.setAttribute("data-open", "false")
+        } else{
+          el.setAttribute("data-open", "true")
+        }  
+      } else {
+        el.setAttribute("data-open", "false")
+      }
+    })
+  }else{
+    //Toggle open or close elements (up to 3 elements open at the time) when width is over 576px
+    if(codeBtn.getAttribute("data-open") === "false"){
+      codeElements.forEach(el =>{
+        el.setAttribute("data-open", "true")
+      });
+    } else{
+      codeElements.forEach(el =>{
+        el.setAttribute("data-open", "false")
+      });
+    }
+  }
 
+  //check if there is an open element
+  let openElements = false;
+  codeEditorBtns.forEach(btn =>{
+    if(btn.getAttribute("data-open") === "true"){
+      openElements =true;
+    } 
+  })
 
+  if(openElements){
+    codeEditor.setAttribute("data-open", "true");
+  } else {
+    codeEditor.setAttribute("data-open", "false");
+  }
+};
 
+function codeEditorWindowResize(mediumMediaQuery){
+  let openElements = false;
+  codeEditorBtns.forEach(btn =>{
+    if(btn.getAttribute("data-open") === "true"){
+      openElements =true;
+    } 
+  })
 
-
-// #### CODE EDITOR FUNCTION ####
-function runEditor(iframe, htmlCode, cssCode, jsCode) {
-  const code = `${htmlCode} <style>${cssCode}</style><script>${jsCode}</script>`
-  const iframeContent = iframe.contentWindow.document;
-  iframeContent.open();
-  iframeContent.write(code);
-  iframeContent.close();
+  if(!mediumMediaQuery.matches && codeEditor !== null && openElements){
+    document.querySelectorAll("[data-code-type]").forEach(el =>{
+      if(el.getAttribute("data-code-type") === "html"){
+        el.setAttribute("data-open", "true")
+      } else {
+        el.setAttribute("data-open", "false")
+      }
+    })
+  }
 }
+
+
+
+//#### Init post detail functions ####
+function postDetailFunctions(){
+  if(codeEditor !== null){
+    let iframe = document.querySelector(".code-editor-iframe iframe");
+    let htmlCode = document.getElementById("htmlCode").value
+    let cssCode = document.getElementById("cssCode").value
+    let jsCode = document.getElementById("jsCode").value
+
+    console.log(htmlCode)
+    console.log(cssCode)
+    console.log(jsCode)
+
+    runEditor(iframe, htmlCode, cssCode, jsCode)
+    
+  }
+}
+
+document.addEventListener('DOMContentLoaded', postDetailFunctions);
+
+
+mediumMediaQuery.addEventListener('change', codeEditorWindowResize);
+
+
+
+
+
+
+
