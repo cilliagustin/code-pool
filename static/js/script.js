0ae00715caca906e3ad42e111563ee216349920c
Agustin cilli <cilli.agustin@gmail.com>
2023-01-07 19:25:56 +0000

    Edit styling + add functions to preview code and create slug on add_post template

--- static/js/script.js
+++ static/js/script.js
@@ -5,6 +5,9 @@ const navButton = document.getElementById("hamb");
 const overlay = document.getElementById("overlay");
 const codeEditor = document.getElementById("code-editor");
 const codeEditorBtns = document.querySelectorAll(".code-editor-btn");
+const previewCode = document.getElementById("preview-code");
+const postTitle = document.getElementById("post-title");
+const postSlug = document.getElementById("post-slug");
 const authFormInputs = document.querySelectorAll(".auth-card form p input[type='text'], .auth-card form p input[type='password'], .auth-card form p input[type='email']")
 
 
@@ -225,6 +228,47 @@ function postDetailFunctions(){
 }
 
 document.addEventListener('DOMContentLoaded', postDetailFunctions);
-
-
 mediumMediaQuery.addEventListener('change', codeEditorWindowResize);
+
+
+
+
+// ##### CREATE/EDIT POSTS #####
+
+// #### SLUGIFY ####
+function slugify(title, slug) {
+  let str = title.value;
+
+  str = str.replace(/^\s+|\s+$/g, ''); // trim
+  str = str.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  let from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+  let to   = "aaaaeeeeiiiioooouuuunc------";
+  for (let i=0, l=from.length ; i<l ; i++) {
+      str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+  }
+
+  str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+      .replace(/\s+/g, '-') // collapse whitespace and replace by -
+      .replace(/-+/g, '-'); // collapse dashes
+
+  slug.value = str;
+}
+
+postTitle.addEventListener('input', ()=>{
+  slugify(postTitle, postSlug)
+})
+
+
+// #### PREVIEW CODE ####
+previewCode.addEventListener('click', (e)=>{
+  e.preventDefault()
+  document.querySelector('.create-edit-iframe-container').classList.add('open');
+
+  let htmlCode = document.querySelector('.HTML-code').value;
+  let cssCode = document.querySelector('.CSS-code').value;
+  let jsCode = document.querySelector('.JS-code').value;
+  let iframe = document.querySelector('iframe');
+  runEditor(iframe, htmlCode, cssCode, jsCode);
+})
