f6517cbeba2e283088856b293bd1b8fa1ea537fe
Agustin cilli <cilli.agustin@gmail.com>
2023-01-15 16:02:29 +0000

    Create view to filter posts y category + wire url

--- forum/urls.py
+++ forum/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.IndexPostList.as_view(), name='home'),
     path('posts', views.AllPostList.as_view(), name='posts'),
     path('posts/<slug:slug>', views.PostDetail.as_view(), name='post_detail'),
+    path('posts/category/<str:category>', views.FilterCategory.as_view(), name='category'),
     path('posts/edit_post/<slug:slug>', views.EditPost.as_view(), name='edit_post'),
     path('posts/delete_post/<slug:slug>', views.DeletePost.as_view(), name='delete_post'),
     path('new_post/', views.CreatePost.as_view(), name='add_post'),
--- forum/views.py
+++ forum/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from django.urls import reverse_lazy
-from .models import Post
+from .models import Post, Category
 from .forms import CommentForm, PostForm, EditForm
 
 
@@ -16,35 +16,6 @@ class AllPostList(generic.ListView):
     queryset = Post.objects.order_by('-created_on')
     template_name = 'posts.html'
     paginate_by = 6
-
-
-class Canvas(generic.ListView):
-    model = Post
-    template_name = 'canvas.html'
-
-
-class CreatePost(generic.CreateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'add_post.html'
-
-    def form_valid(self, form):
-        self.object = form.save(commit=False)
-        self.object.author = self.request.user
-        self.object.save()
-        return super().form_valid(form)
-
-
-class EditPost(generic.UpdateView):
-    model = Post
-    form_class = EditForm
-    template_name = 'edit_post.html'
-
-
-class DeletePost(generic.DeleteView):
-    model = Post
-    template_name = 'delete_post.html'
-    success_url = reverse_lazy('home')
 
 
 class PostDetail(View):
@@ -91,3 +62,47 @@ class PostDetail(View):
                 "comment_form": CommentForm()
             },
         )
+
+
+class FilterCategory(generic.ListView):
+    model = Post
+    template_name = 'categories.html'
+    paginate_by = 6
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = self.kwargs['category']
+        return context
+
+    def get_queryset(self):
+        category = get_object_or_404(Category, name=self.kwargs['category'])
+        return Post.objects.filter(category=category)
+
+
+class CreatePost(generic.CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'add_post.html'
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.author = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
+
+class EditPost(generic.UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'edit_post.html'
+
+
+class DeletePost(generic.DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
+
+
+class Canvas(generic.ListView):
+    model = Post
+    template_name = 'canvas.html'
