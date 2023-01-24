eb683341959e1a3bfd258371e2e20eb1ef8f2efd
Agustin cilli <cilli.agustin@gmail.com>
2023-01-24 09:05:45 +0000

    Fix lines too long

--- forum/forms.py
+++ forum/forms.py
@@ -8,30 +8,56 @@ choice_list = [choice for choice in choices]
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'slug', 'category', 'html_content', 'css_content','js_content')
+        fields = (
+            'title', 'slug', 'category', 'html_content',
+            'css_content', 'js_content')
 
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control', 'id': 'post-title'}),
-            'slug': forms.TextInput(attrs={'class': 'form-control', 'id': 'post-slug'}),
-            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
-            'html_content': forms.Textarea(attrs={'class': 'form-control HTML-code', 'placeholder': 'Paste here your HTML code.'}),
-            'css_content': forms.Textarea(attrs={'class': 'form-control CSS-code', 'placeholder': 'Paste here your CSS code.'}),
-            'js_content': forms.Textarea(attrs={'class': 'form-control JS-code', 'placeholder': 'Paste here your JS code. (Optional)'}),
+            'title': forms.TextInput(attrs={
+                'class': 'form-control', 'id': 'post-title'}),
+            'slug': forms.TextInput(attrs={
+                'class': 'form-control', 'id': 'post-slug'}),
+            'category': forms.Select(choices=choice_list, attrs={
+                'class': 'form-control'}),
+            'html_content': forms.Textarea(attrs={
+                'class': 'form-control HTML-code',
+                'placeholder': 'Paste here your HTML code.'}),
+            'css_content': forms.Textarea(attrs={
+                'class': 'form-control CSS-code',
+                'placeholder': 'Paste here your CSS code.'}),
+            'js_content': forms.Textarea(attrs={
+                'class': 'form-control JS-code',
+                'placeholder': 'Paste here your JS code. (Optional)'}),
         }
 
 
 class EditForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'slug', 'category', 'html_content', 'css_content','js_content')
+        fields = (
+            'title', 'slug', 'category', 'html_content', 'css_content',
+            'js_content'
+            )
 
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control', 'id': 'post-title'}),
-            'slug': forms.TextInput(attrs={'class': 'form-control', 'id': 'post-slug'}),
-            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
-            'html_content': forms.Textarea(attrs={'class': 'form-control HTML-code', 'placeholder': 'Paste here your HTML code.'}),
-            'css_content': forms.Textarea(attrs={'class': 'form-control CSS-code', 'placeholder': 'Paste here your CSS code.'}),
-            'js_content': forms.Textarea(attrs={'class': 'form-control JS-code', 'placeholder': 'Paste here your JS code. (Optional)'}),
+            'title': forms.TextInput(
+                attrs={'class': 'form-control', 'id': 'post-title'}),
+            'slug': forms.TextInput(
+                attrs={'class': 'form-control', 'id': 'post-slug'}),
+            'category': forms.Select(
+                choices=choice_list, attrs={'class': 'form-control'}),
+            'html_content': forms.Textarea(attrs={
+                    'class': 'form-control HTML-code',
+                    'placeholder': 'Paste here your HTML code.'
+                    }),
+            'css_content': forms.Textarea(attrs={
+                    'class': 'form-control CSS-code',
+                    'placeholder': 'Paste here your CSS code.'
+                    }),
+            'js_content': forms.Textarea(attrs={
+                    'class': 'form-control JS-code',
+                    'placeholder': 'Paste here your JS code. (Optional)'
+                    }),
         }
 
 
