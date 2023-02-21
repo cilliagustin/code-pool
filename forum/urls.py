f9a06becaa026d9df3824a31e3262235d30701c7
Agustin cilli <cilli.agustin@gmail.com>
2023-02-21 09:15:57 +0000

    Change URL for delete comment view

--- forum/urls.py
+++ forum/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('posts', views.AllPostList.as_view(), name='posts'),
     path('posts/<slug:slug>', views.PostDetail.as_view(), name='post_detail'),
     path(
-        'category/delete_comment/<int:pk>',
+        'posts/<slug:slug>/delete_comment/<int:pk>',
         views.DeleteComment.as_view(), name='delete_comment'),
     path(
         'approve_comment/<int:pk>/', views.ApproveComment.as_view(),
